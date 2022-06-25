a0753dad6b551153e627883e47af5e169e1477b8
abhavgoel <goelabhav2002@gmail.com>
2022-06-26 02:51:08 +0530

    sliding

---
+++ Sliding Window/first_negative_in_every_window.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sliding(int arr[],int n,int k)
+{
+    list<int> l;
+    vector<int>v;
+    int i=0,j=0;
+    while(j<n)
+    {
+        if(arr[j]<0)
+            l.push_back(arr[j]);
+        
+
+            if(j-i+1<k)
+                j++;
+
+            else if(j-i+1==k)
+            {
+                if(l.size()==0)
+                    v.push_back(0);
+                
+                else
+                {
+                    v.push_back(l.front());
+                    if(arr[i]==l.front())
+                        l.pop_front();
+                    
+                    i++;
+                    j++;
+                    
+                }
+            }
+
+    }
+     for(auto i:v)
+     {
+        cout<<i<<" ";
+     }
+     
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int k;
+    cin>>k;
+    // vector<int> vv;
+    // vv=sliding(arr,n,k);
+    // for(auto i:vv)
+    // {
+    //     cout<<i<<" ";
+    // }
+    sliding(arr,n,k);
+}
---
+++ Sliding Window/max_sum_subarray_sizek.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int slidingWindow(int arr[],int n,int k)
+{
+    int sum=0;
+    int mx=INT_MIN;
+    int i,j=0;
+    while (j<n)
+    {
+        sum+=arr[j];
+        if(j-i+1<k)
+        j++;
+        else if(j-i+1==k)
+        {
+            mx=max(mx,sum);
+            sum=sum-arr[i];
+            i++;
+            j++;
+        }
+    }
+    return mx;
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int k;
+    cin>>k;
+    cout<<slidingWindow(arr,n,k);
+}
