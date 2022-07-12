376d9d8d4862ff1e118c5708582ce8a2ef7f2e6c
abhavgoel <goelabhav2002@gmail.com>
2022-07-13 04:01:41 +0530

    exe files remove

--- Strings/check_anagram.cpp
+++ Strings/check_anagram.cpp
@@ -1,2 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+bool isAnagram(string s, string ss)
+{
+    if(s.length()!=ss.length())
+    return false;
+    map<char,int> m;
+    for(int i=0;i<s.length();i++)
+    {
+        m[s[i]]++;
+        m[ss[i]]--;
+    }
+    for(auto i:m)
+    if(i.second!=0)
+    return false;
+
+    return true;
+
+    
+}
+int main()
+{
+    string s;
+    cin>>s;
+    string ss;
+    cin>>ss;
+    cout<<isAnagram(s,ss);
+}
