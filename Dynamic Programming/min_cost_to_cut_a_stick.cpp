8cb57c1a4f40e653d64f12146ac7d8510aed7dca
Abhav <goelabhav2002@gmail.com>
2023-04-01 03:57:11 +0530

    burst balloons

---
+++ Dynamic Programming/min_cost_to_cut_a_stick.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+long long helper(int i,int j,vector<int>&cuts, vector<vector<int>>&dp)
+{
+    if(i>j)
+    return 0;
+    if(dp[i][j]!=-1)return dp[i][j];
+    long long mini=1e8;
+    for(int ind=i;ind<=j;ind++)
+    {
+        long long cost=cuts[j+1]-cuts[i-1] 
+        + helper(i,ind-1,cuts,dp) + helper(ind+1,j,cuts,dp);
+        mini=min(mini,cost);
+    }
+    return dp[i][j] = mini;
+}
+    int minCost(int n, vector<int>& cuts) {
+        int c=cuts.size();
+        cuts.push_back(n);
+        cuts.push_back(0);
+        sort(cuts.begin(),cuts.end());
+        // vector<vector<int>>dp(c+1,vector<int>(c+1,-1));
+        
+        // for(auto i:cuts)cout<<i<<" ";
+        // return helper(1,c,cuts,dp);
+
+        vector<vector<int>>dp(c+2,vector<int>(c+2,0));
+
+        // i goes from 1->n....and j from n->1...so in tabulation order would be reversed
+
+        for(int i=c;i>=1;i--)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                if(i>j)continue;
+
+                long long mini=1e8;
+                for(int ind=i;ind<=j;ind++)
+                {
+                    long long cost=cuts[j+1]-cuts[i-1] 
+                    + dp[i][ind-1] + dp[ind+1][j];
+                    mini=min(mini,cost);
+                }
+                dp[i][j] = mini;
+
+            }
+        }
+        return dp[1][c];
+
+    }
+};
