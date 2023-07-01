332a2801b363ccee68a03786171d01d64286fb07
Abhav <goelabhav2002@gmail.com>
2023-07-01 18:39:49 +0530

    kruskal algo

--- bitwise_operators/xor.cpp
+++ bitwise_operators/xor.cpp
@@ -13,3 +13,17 @@ using namespace std;
 // a ^ a = 0
 
 
+int main()
+{
+    //if numebr of set bits in A is x, and B is y
+    //then number of set bits in A^B is even is x+y is even and odd if x+y is odd
+
+    //lets say A = 1100110001
+    //B = 0100110010
+
+    //x = 5, y=4 , x+y = 9
+    //then let the number of bits be k that are same in the set bits of A and B
+    //then total set bits in A^B is x+y-2*k -->because that bit will be minused from both numbers 
+
+
+}
