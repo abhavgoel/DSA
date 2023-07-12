f3646a8ce8a4b29d7f37fc6af892eb8152a1eaf6
Abhav <goelabhav2002@gmail.com>
2023-07-12 17:53:37 +0530

    tree updated

--- Binary tree/diameter.cpp
+++ Binary tree/diameter.cpp
@@ -1,43 +1,60 @@
-#include<bits/stdc++.h>
-using namespace std;
-struct node
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int maxi=-1;
+
+//-------------O(n^2)----------------------------
+// int height(TreeNode *root)
+// {
+//     if (root==NULL)
+//     return 0;
+//     return max(height(root->left),height(root->right))+1;
+// }
+// void maxDiameter(TreeNode* root)
+// {
+//     if(root==NULL)
+//     return ;
+
+//     int lh = height(root->left);
+//     int rh = height(root->right);
+
+//     // int maxi = -1;
+//     maxi = max(maxi,lh+rh);
+
+//     maxDiameter(root->left);
+//     maxDiameter(root->right);
+
+// }
+
+//------------O(n)-------------------------
+
+int maxDia(TreeNode* root,int &diameter)
 {
-    int key;
-    node *left;
-    node *right;
-    node(int k)
-    {
-        key=k;
-        left=right=NULL;
+    if(root==NULL)
+    return 0;
+
+
+    int lh = maxDia(root->left,diameter);
+    int rh = maxDia(root->right,diameter);
+
+    diameter = max(diameter,lh+rh);
+
+    return 1+max(lh,rh);
+}
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter=0;
+        maxDia(root,diameter);
+        return diameter;
     }
 };
-
-int height(node *root)
-    {
-        if(root==NULL)
-            return 0;
-        int lh=height(root->left);
-        int rh=height(root->right);
-        
-        int ans= max(lh,rh)+1;
-        return ans;
-    }
-    int diameterOfBinaryTree(node* root) {
-        if(root==NULL)
-            return 0;
-        int d1=1+height(root->left)+height(root->right);
-        
-        int d2=diameterOfBinaryTree(root->left);
-        int d3=diameterOfBinaryTree(root->right);
-        return max(d1,max(d2,d3));
-    }
-    int main()
-{
-    node *root=new node(10);
-    root->left=new node(20);
-    root->right=new node(30);
-    root->left->left=new node(40);
-    root->left->right=new node(50);
-    
-    cout<<diameterOfBinaryTree(root);
-}
