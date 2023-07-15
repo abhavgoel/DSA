800d224bfb2fe39b271452f781efe8e86ab7c1c6
Abhav <goelabhav2002@gmail.com>
2023-07-15 18:37:05 +0530

    convert tree to children sum property

---
+++ Binary tree/convert_tree_to_children_sum_property.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+    void changeTree(TreeNode* root)
+    {
+        if(root==NULL)
+        return;
+
+        int child =0;
+
+        if(root->left)
+        child+=root->left->val;
+        if(root->right)
+        child+=root->right->val
+
+        if(child>=root->val)
+        {
+            root->val=child;
+        }
+        else
+        {
+            if(root->left)
+            root->left->val=root->val;
+            else if(root->right)
+            root->right->val=root->val;
+
+        }
+        changeTree(root->left);
+        changeTree(root->right);
+
+        int total=0;
+        if(root->left)tot+=root->left->val;
+        if(root->right)tot+=root->right->val;
+
+        if(root->left || root->right)root->val=total;
+    }
+};
