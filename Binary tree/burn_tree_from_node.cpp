974a4cc281863195a4cd61395717e466e4a16b67
Abhav <goelabhav2002@gmail.com>
2023-07-16 18:57:48 +0530

    burn binary tree from node bfs solution

--- Binary tree/burn_tree_from_node.cpp
+++ Binary tree/burn_tree_from_node.cpp
@@ -1,108 +1,79 @@
-#include<bits/stdc++.h>
-using namespace std;
-struct node
-{
-    int key;
-    node *left;
-    node *right;
-    node(int k)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mapParents(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&parent,int start)
     {
-        key=k;
-        left=right=NULL;
+        queue<TreeNode*>q;
+        q.push(root);
+        TreeNode *startNode;
+        while(q.empty()==false)
+        {
+            TreeNode* curr = q.front();
+            q.pop();
+            if(curr->val==start)
+            startNode = curr;
+
+            if(curr->left)
+            {
+                q.push(curr->left);
+                parent[curr->left] = curr;
+
+            }
+            if(curr->right)
+            {
+                q.push(curr->right);
+                parent[curr->right] = curr;
+                
+            }
+        }
+        return startNode;
+    }
+    int amountOfTime(TreeNode* root, int start) {
+        unordered_map<TreeNode*,TreeNode*>parent;
+        TreeNode* startNode = mapParents(root,parent,start);
+
+        queue<pair<TreeNode*,int>>q;
+        map<TreeNode* ,int>vis;
+        int time = 0;
+        //start node needed , but we are given start value
+        q.push({startNode,0});
+        vis[startNode]=1;
+        while(q.empty()==false)
+        {
+            TreeNode *curr=q.front().first;
+            int d = q.front().second;
+            q.pop();
+            time = d;
+
+            if(curr->left && vis[curr->left]==0)
+            {
+                q.push({curr->left,d+1});
+                vis[curr->left]=1;
+
+            }
+            if(curr->right && vis[curr->right]==0)
+            {
+                q.push({curr->right,d+1});
+                vis[curr->right]=1;
+                
+            }
+            if(parent.find(curr)!=parent.end() && vis[parent[curr]]==0)
+            {
+                q.push({parent[curr],d+1});
+                vis[parent[curr]]=1;
+            }
+
+        }
+        return time;
     }
 };
-int findMaxDistance(map<node*,node*> &mp,node *target)
-{
-    
-    queue<node*>q;
-    q.push(target);
-    map<node*,int>visited;
-
-    visited[target] =1;
-    int maxi=0;
-
-    while(!q.empty())
-    {
-        int flag=0;
-        int n=q.size();
-        for(int i=0;i<n;i++)
-        {
-            node *curr=q.front();
-            q.pop();
-
-            if(curr->left && !visited[curr->left])
-            {
-                flag=1;
-                visited[curr->left] =1 ;
-                q.push(curr->left);
-            }
-
-            if(curr->right && !visited[curr->right])
-            {
-                flag=1;
-                visited[curr->right]=1;
-                q.push(curr->right);
-            }
-
-            if(mp[curr] && !visited[mp[curr]])
-            {
-                flag=1;
-                visited[mp[curr]]=1;
-                q.push(mp[curr]);
-            }
-
-        }
-        if(flag==1)
-        maxi++;
-    }
-    return maxi;
-
-
-
-}
-
-node *bfsToMapParents(node *root,map<node*,node*>&mp,int start)
-{
-    queue<node*>q;
-    q.push(root);
-    node *res;
-    while(!q.empty())
-    {
-        node *curr=q.front();
-        q.pop();
-        if(curr->key==start)
-        res=curr;
-
-        if(curr->left)
-        {
-            mp[curr->left]=curr;
-            q.push(curr->left);
-        }
-        if(curr->right)
-        {   
-            mp[curr->right]=curr;
-            q.push(curr->right);
-        }
-    }
-    return res;
-}
-int main()
-{
- node *root=new node(10);
-    root->left=new node(20);
-    root->right=new node(30);
-    root->left->left=new node(40);
-    root->left->right=new node(50);
-    cout<<"data of node to burn tree from ";
-    int start;
-    cin>>start;
-
-
-    map<node*,node*> mp;
-
-    node* target = bfsToMapParents(root,mp,start);
-
-    int maxi=findMaxDistance(mp,target);
-    cout<<maxi;
-
-}
