0f2148f5f39c913c22249ef14e09fab6678c1fc2
Abhav <goelabhav2002@gmail.com>
2023-07-28 00:13:39 +0530

    rec0ver bst

---
+++ Binary tree/recover_bst.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* prev;
+TreeNode* first;
+TreeNode* middle;
+TreeNode* last;
+
+void inorder(TreeNode* root){
+    if(root==NULL)
+    return;
+
+    inorder(root->left);
+
+    if(prev!=NULL && (root->val < prev->val))
+    {
+        if(first==NULL)//when 1st time a discrepancy is found
+        {
+            first = prev;
+            middle = root;
+        }
+        else//2nd element smaller than previous
+        {
+            last = root;
+        }
+    }
+
+    prev = root;
+    inorder(root->right);
+}
+
+    void recoverTree(TreeNode* root) {
+    first=middle=last=NULL;
+    prev = new TreeNode(INT_MIN);
+
+    inorder(root);
+
+    if(first && last)
+    swap(first->val, last->val);
+
+    else if(first && middle)
+    swap(first->val, middle->val);
+
+    
+    }
+};
