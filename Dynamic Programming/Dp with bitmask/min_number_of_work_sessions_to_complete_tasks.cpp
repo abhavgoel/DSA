89d2d39208dd98d18b8addd1df160091903dbbde
Abhav <goelabhav2002@gmail.com>
2023-08-11 18:24:21 +0530

    q3 dp with bitmask

---
+++ Dynamic Programming/Dp with bitmask/min_number_of_work_sessions_to_complete_tasks.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+int final_mask;
+vector<vector<int>>dp;
+int helper(int time,int mask,int sessionTime,vector<int>&tasks)
+{
+    if(mask==final_mask)
+    {
+        return 1;
+    }
+    if(dp[time][mask]!=-1)
+    return dp[time][mask];
+    int ans=1e9;
+    for(int j=0;j<tasks.size();j++)
+    {
+        if(mask & (1<<j))continue;
+
+        int pickInThisSession = 1e9;
+
+        if(time+tasks[j]<=sessionTime)
+        pickInThisSession = helper(time+tasks[j],mask | (1<<j),sessionTime,tasks);
+
+        int pickInNextSession = 1 + helper(tasks[j], mask | (1<<j),sessionTime,tasks);
+        
+        ans=min(ans,min(pickInThisSession,pickInNextSession));
+    }
+    return dp[time][mask] = ans;
+    
+
+
+}
+    int minSessions(vector<int>& tasks, int sessionTime) {
+        final_mask = (1<<tasks.size())-1;
+        dp.resize(sessionTime+1,vector<int>(1<<tasks.size(),-1));
+        return helper(0,0,sessionTime,tasks);
+
+    }
+};
