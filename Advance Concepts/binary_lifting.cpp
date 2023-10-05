96898342fa0ebebf4fa0dc9f897c72508a71f48f
Abhav Goel <goelabhav2002@gmail.com>
2023-10-05 17:31:06 +0530

    update

--- Advance Concepts/binary_lifting.cpp
+++ Advance Concepts/binary_lifting.cpp
@@ -5,7 +5,7 @@ public:
     int LOG;
     TreeAncestor(int n, vector<int>& parent) {
         LOG = ceil(log2(n));
-        up.resize(n,vector<int>(LOG,-1));
+        up.resize(n,vector<int>(LOG+1,-1));
         for(int i=0;i<n;i++)
         {
             up[i][0]=parent[i];//2^0, i.e first ancestor of a node is its parent
@@ -28,8 +28,13 @@ public:
         //now we need to process queries as powers of 2, 
         //lets say 5th ancestor --> binary of 5 = 101, in the table we have answers of 2^i th ancestors
         //so 5 can be rewritten as 2^2 + 2^0, so we calculate ans according to set bit
+
+        if(k==1)
+        return up[node][0];
+
+       
         int ans=node;
-        for(int j=0;j<LOG;j++) //O(log(N))
+        for(int j=0;j<=LOG;j++) //O(log(N))
         {
             if(k&(1<<j))
             {
@@ -40,7 +45,12 @@ public:
                 k-=(1<<j);
             }
         }
-        return ans==node?-1:ans;
+        return ans;
     }
 };
 
+/**
+ * Your TreeAncestor object will be instantiated and called as such:
+ * TreeAncestor* obj = new TreeAncestor(n, parent);
+ * int param_1 = obj->getKthAncestor(node,k);
+ */
