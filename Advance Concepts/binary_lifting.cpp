2787ed10c4a1e2bf93d05239c5ed6ec391b2ddc4
Abhav Goel <goelabhav2002@gmail.com>
2023-10-05 06:34:57 +0530

    binary lifting

---
+++ Advance Concepts/binary_lifting.cpp
@@ -0,0 +1,46 @@
+class TreeAncestor {
+public:
+ vector<vector<int>>up;//table to calculate ancestors, N x log(N)
+    //up array is defined as up[i][j] --> 2^j th ancestor of ith node
+    int LOG;
+    TreeAncestor(int n, vector<int>& parent) {
+        LOG = ceil(log2(n));
+        up.resize(n,vector<int>(LOG,-1));
+        for(int i=0;i<n;i++)
+        {
+            up[i][0]=parent[i];//2^0, i.e first ancestor of a node is its parent
+        }   
+
+        for(int j=1;j<LOG;j++)
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(up[i][j-1]!=-1)
+                up[i][j] = up[up[i][j-1]][j-1];
+                else 
+                up[i][j]=-1;
+                
+            }
+        }
+    }
+    
+    int getKthAncestor(int node, int k) {
+        //now we need to process queries as powers of 2, 
+        //lets say 5th ancestor --> binary of 5 = 101, in the table we have answers of 2^i th ancestors
+        //so 5 can be rewritten as 2^2 + 2^0, so we calculate ans according to set bit
+        int ans=node;
+        for(int j=0;j<LOG;j++) //O(log(N))
+        {
+            if(k&(1<<j))
+            {
+                ans=up[ans][j];
+                if(ans==-1)
+                break;
+
+                k-=(1<<j);
+            }
+        }
+        return ans==node?-1:ans;
+    }
+};
+
